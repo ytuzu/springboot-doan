54ed9f9abd3bfa589d97ec7d537a8bd105787642
Nguyễn Huỳnh Nhật Quang <48125913+ytuzu@users.noreply.github.com>
2020-12-17 22:10:10 +0700

    Update Application.java

--- src/main/java/com/shop/api/Application.java
+++ src/main/java/com/shop/api/Application.java
@@ -9,11 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("");
-		frame.setSize(200, 200);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
 		SpringApplication.run(Application.class, args);
 	}
 }
