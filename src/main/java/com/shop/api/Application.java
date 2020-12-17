01c94e30c05eaa7565d8d359faf64200471739c0
Nguyễn Huỳnh Nhật Quang <nguyenhnhatquang@gmail.com>
2020-12-17 22:03:59 +0700

    first commit

---
+++ src/main/java/com/shop/api/Application.java
@@ -0,0 +1,19 @@
+package com.shop.api;
+
+import javax.swing.JFrame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("");
+		frame.setSize(200, 200);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		SpringApplication.run(Application.class, args);
+	}
+}
