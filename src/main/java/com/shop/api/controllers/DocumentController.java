b31e7550362bd9db7b45b7b4e32a1fc26564288a
Nguyễn Huỳnh Nhật Quang <nguyenhnhatquang@gmail.com>
2020-12-18 22:40:01 +0700

    add document

---
+++ src/main/java/com/shop/api/controllers/DocumentController.java
@@ -0,0 +1,27 @@
+package com.shop.api.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+
+@RestController
+@RequestMapping
+public class DocumentController {
+	@GetMapping({"/doc","/", "/document"})
+	public @ResponseBody Object documentShow() {
+		Resource resource = new ClassPathResource("/static/json/document.json");
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(resource.getInputStream(), Object.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+	}
+}
--- src/main/java/com/shop/api/security/WebSecurityConfig.java
+++ src/main/java/com/shop/api/security/WebSecurityConfig.java
@@ -56,9 +56,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/v1/auth/**").permitAll()
-			.antMatchers("/").permitAll()
+			.antMatchers("/document").permitAll()
 			.antMatchers("/doc").permitAll()
-			.antMatchers("/api/test/**").permitAll()
+			.antMatchers("/").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
