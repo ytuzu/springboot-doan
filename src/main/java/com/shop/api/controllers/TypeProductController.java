7493c473764bef6184ad620b34a29cd17984944f
Nguyễn Huỳnh Nhật Quang <nguyenhnhatquang@gmail.com>
2020-12-29 14:26:34 +0700

    abc

--- src/main/java/com/shop/api/controllers/TypeProductController.java
+++ src/main/java/com/shop/api/controllers/TypeProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.shop.api.payloads.responses.MessageResponse;
 import com.shop.api.payloads.responses.TypeProductResponse;
 import com.shop.api.services.TypeProductService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/typeproduct")
 public class TypeProductController {
